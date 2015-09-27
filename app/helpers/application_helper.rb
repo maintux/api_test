18922ab66b011e48f61160b42589a89e26a989aa
Massimo Maino <maintux@gmail.com>
2015-09-27 13:38:05 +0200

    helper for errors in form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def has_errors?(object,attribute)
+    object.respond_to?(:errors) && !(attribute.nil? || object.errors[attribute].empty?)
+  end
+
+  def get_errors_for(object,attribute)
+    object.errors[attribute].try(:join, ', ') || object.errors[attribute].try(:to_s)
+  end
+
 end
